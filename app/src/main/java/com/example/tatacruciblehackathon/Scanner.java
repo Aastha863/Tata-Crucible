5d4c61c168e0aa31f0f9dc63b2d65427e8137e40
Aastha863 <sinha2001aastha@gmail.com>
2021-06-13 01:51:37 +0530

    Final Repository

--- app/src/main/java/com/example/tatacruciblehackathon/Scanner.java
+++ app/src/main/java/com/example/tatacruciblehackathon/Scanner.java
@@ -2,6 +2,8 @@ package com.example.tatacruciblehackathon;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -15,13 +17,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
 import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.Reader;
 
 public class Scanner extends AppCompatActivity {
     CodeScanner cs;
@@ -56,16 +67,23 @@ public class Scanner extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 cs.startPreview();
+                resultdata.setText("Url link");
             }
         });
         visit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Intent.ACTION_VIEW);
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
 
-                intent.setData(Uri.parse(r));
+                    intent.setData(Uri.parse(r));
 
-                startActivity(intent);
+                    startActivity(intent);
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(Scanner.this, "Requested URL not found",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -75,7 +93,88 @@ public class Scanner extends AppCompatActivity {
         super.onResume();
         requestForCamera();
     }
+    public void gallery(View view){
+        //pick image from galary
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, 1000);
+    }
+    @Override
 
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+
+
+
+        if (resultCode == RESULT_OK) {
+
+            try {
+
+                final Uri imageUri = data.getData();
+
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                try {
+
+                    Bitmap bMap = selectedImage;
+
+                    String contents = null;
+
+
+
+                    int[] intArray = new int[bMap.getWidth()*bMap.getHeight()];
+
+                    bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
+
+
+
+                    LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
+
+                    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+
+
+                    MultiFormatReader reader = new MultiFormatReader();
+
+                    Result result = reader.decode(bitmap);
+
+                    contents = result.getText();
+
+                    resultdata.setText(contents);
+                    r=contents;
+
+
+                }catch (Exception e){
+
+                    e.printStackTrace();
+                    Toast.makeText(Scanner.this,"Given image is not a QR code thus can't be scanned be scanned",Toast.LENGTH_SHORT).show();
+
+                }
+
+                //  image_view.setImageBitmap(selectedImage);
+
+            } catch (FileNotFoundException e) {
+
+                e.printStackTrace();
+
+                Toast.makeText(Scanner.this, "Something went wrong", Toast.LENGTH_LONG).show();
+
+            }
+
+
+
+        }else {
+
+            Toast.makeText(Scanner.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+
+        }
+
+    }
     private void requestForCamera() {
         Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
             @Override
