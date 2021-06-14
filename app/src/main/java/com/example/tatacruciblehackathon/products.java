13f0ece371986d778d1fb68b3f1a89fdec9c0eb6
Aastha863 <sinha2001aastha@gmail.com>
2021-06-14 12:51:23 +0530

    Final app added

--- app/src/main/java/com/example/tatacruciblehackathon/products.java
+++ app/src/main/java/com/example/tatacruciblehackathon/products.java
@@ -19,15 +19,15 @@ public class products extends AppIntro {
         super.onCreate(savedInstanceState);
 
         addSlide(AppIntroFragment.newInstance(
-                "Explore our products", "The best one at the Tata Steel", R.drawable.tata_steel, Color.rgb(0, 120, 0), Color.WHITE, Color.WHITE));
+                "Explore our products", "The best one at the Tata Steel", R.drawable.tata_steel, Color.rgb(0, 0, 120), Color.WHITE, Color.WHITE));
         addSlide(AppIntroFragment.newInstance(
                 "Bars", "Tata Steel to sell long products", R.drawable.bars, Color.BLUE));
         addSlide(AppIntroFragment.newInstance(
-                "Roof Junction", "Strong and tough", R.drawable.roof_junction, Color.rgb(150, 0, 0), Color.rgb(255, 255, 255), Color.rgb(255, 255, 255)));
+                "Roof Junction", "Strong and tough", R.drawable.roof_junction, Color.rgb(150, 0, 0), Color.rgb(0, 0, 120), Color.rgb(255, 255, 255)));
         addSlide(AppIntroFragment.newInstance(
-                "Tata Shaktee Ridges", "Effective steel solutions of different structures", R.drawable.tata_shaktee_ridges, Color.rgb(150, 0, 120)));
+                "Tata Shaktee Ridges", "Effective steel solutions of different structures", R.drawable.tata_shaktee_ridges, Color.rgb(0, 0, 120)));
         addSlide(AppIntroFragment.newInstance(
-                "Galvana", "Tata Steel introduces galvana", R.drawable.galvona, Color.rgb(120, 120, 0)));
+                "Galvana", "Tata Steel introduces galvana", R.drawable.galvona, Color.rgb(0, 0, 120)));
     }
     @Override
     protected void onSkipPressed(@Nullable Fragment currentFragment) {
